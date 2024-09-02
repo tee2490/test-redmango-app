cbb11fe60c0655cd6c0b6ac9adaee09981e228c1
Teeradet <tee2490@hotmail.com>
2024-09-02 16:53:06 +0700

    Section 05 Client : Shopping Cart Functional

--- src/components/cart/CartSummary.tsx
+++ src/components/cart/CartSummary.tsx
@@ -3,11 +3,16 @@ import { Text, View, TouchableOpacity } from "react-native";
 import { SwipeListView } from "react-native-swipe-list-view";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "./CartSummary.style";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { cartItemModel } from "../../interfaces";
 import { RootState } from "../../redux/store";
+import { useUpdateShoppingCartMutation } from "../../redux/apis/shoppingCartApi";
+import { removeFromCart, updateQuantity } from "../../redux/shoppingCartSlice";
+import { userTest } from "../../common/SD";
 
 const CartSummary: React.FC = () => {
+  const dispatch = useDispatch();
+  const [updateShoppingCart] = useUpdateShoppingCartMutation();
   const shoppingCartFromStore: cartItemModel[] = useSelector(
     (state: RootState) => state.shoppingCartStore.cartItems ?? []
   );
@@ -22,11 +27,11 @@ const CartSummary: React.FC = () => {
         <Text style={styles.itemName}>{item.menuItem?.name}</Text>
         <Text style={styles.itemPrice}>${item.menuItem?.price.toFixed(2)}</Text>
         <View style={styles.quantityContainer}>
-          <TouchableOpacity onPress={() => {}}>
+          <TouchableOpacity onPress={() => handleQuantity(-1, item)}>
             <Ionicons name="remove-circle-outline" size={24} color="gray" />
           </TouchableOpacity>
           <Text style={styles.quantityText}>{item?.quantity}</Text>
-          <TouchableOpacity onPress={() => {}}>
+          <TouchableOpacity onPress={() => handleQuantity(1, item)}>
             <Ionicons name="add-circle-outline" size={24} color="gray" />
           </TouchableOpacity>
         </View>
@@ -37,13 +42,51 @@ const CartSummary: React.FC = () => {
     </View>
   );
 
-  const renderHiddenItem = (data: { item: cartItemModel }) => (
+  const renderHiddenItem = ({ item }: { item: cartItemModel }) => (
     <View style={styles.rowBack}>
-      <TouchableOpacity style={styles.backRightBtn} onPress={() => {}}>
+      <TouchableOpacity
+        style={styles.backRightBtn}
+        onPress={() => handleQuantity(0, item)}
+      >
         <Text style={styles.backText}>Remove</Text>
       </TouchableOpacity>
     </View>
   );
+
+  const handleQuantity = (
+    updateQuantityBy: number,
+    cartItem: cartItemModel
+  ) => {
+    if (
+      (updateQuantityBy == -1 && cartItem.quantity == 1) ||
+      updateQuantityBy == 0
+    ) {
+      //remove the item
+
+      //ปรับปรุง DB จริง
+      updateShoppingCart({
+        menuItemId: cartItem.menuItem?.id,
+        updateQuantityBy: 0,
+        userId: userTest,
+      });
+
+      //ปรับปรุง State
+      dispatch(removeFromCart({ cartItem, quantity: 0 }));
+    } else {
+      //update the quantity with the new quantity
+      updateShoppingCart({
+        menuItemId: cartItem.menuItem?.id,
+        updateQuantityBy: updateQuantityBy,
+        userId: userTest,
+      });
+      dispatch(
+        updateQuantity({
+          cartItem,
+          quantity: cartItem.quantity! + updateQuantityBy,
+        })
+      );
+    }
+  };
 
   return (
     <View style={styles.container}>
