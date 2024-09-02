50db130c97cbcb249f276ea0f7131f886f0b8221
Teeradet <tee2490@hotmail.com>
2024-09-02 16:36:00 +0700

    Section 05 Client : Dynamic Data to Shopping Cart Summary

--- src/components/cart/CartSummary.tsx
+++ src/components/cart/CartSummary.tsx
@@ -1,86 +1,45 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  StatusBar,
-  Dimensions,
-} from "react-native";
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
 import { SwipeListView } from "react-native-swipe-list-view";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "./CartSummary.style";
-import {
-  GestureHandlerRootView,
-  ScrollView,
-} from "react-native-gesture-handler";
-
-interface CartItem {
-  id: string;
-  name: string;
-  price: number;
-  quantity: number;
-}
+import { useSelector } from "react-redux";
+import { cartItemModel } from "../../interfaces";
+import { RootState } from "../../redux/store";
 
 const CartSummary: React.FC = () => {
-  const [cartItems, setCartItems] = useState<CartItem[]>([
-    { id: "1", name: "Spring Roll", price: 7.99, quantity: 29 },
-    { id: "2", name: "Idli", price: 8.99, quantity: 6 },
-    { id: "3", name: "Panu Puri", price: 8.99, quantity: 1 },
-    { id: "4", name: "Panu Puri", price: 8.99, quantity: 1 },
-    { id: "5", name: "Spring Roll", price: 7.99, quantity: 29 },
-    { id: "6", name: "Idli", price: 8.99, quantity: 6 },
-    { id: "7", name: "Panu Puri", price: 8.99, quantity: 1 },
-    // { id: "8", name: "Panu Puri", price: 8.99, quantity: 1 },
-  ]);
+  const shoppingCartFromStore: cartItemModel[] = useSelector(
+    (state: RootState) => state.shoppingCartStore.cartItems ?? []
+  );
 
-  const handleDelete = (rowKey: string) => {
-    const newData = cartItems.filter((item) => item.id !== rowKey);
-    setCartItems(newData);
-  };
+  if (!shoppingCartFromStore) {
+    return <Text>Shopping Cart Empty</Text>;
+  }
 
-  const updateQuantity = (id: string, increment: boolean) => {
-    setCartItems((prevItems) =>
-      prevItems.map((item) =>
-        item.id === id
-          ? {
-              ...item,
-              quantity: increment
-                ? item.quantity + 1
-                : Math.max(1, item.quantity - 1),
-            }
-          : item
-      )
-    );
-  };
-
-  const renderItem = ({ item }: { item: CartItem }) => (
+  const renderItem = ({ item }: { item: cartItemModel }) => (
     <View style={styles.rowFront}>
       <View style={styles.itemDetails}>
-        <Text style={styles.itemName}>{item.name}</Text>
-        <Text style={styles.itemPrice}>${item.price.toFixed(2)}</Text>
+        <Text style={styles.itemName}>{item.menuItem?.name}</Text>
+        <Text style={styles.itemPrice}>${item.menuItem?.price.toFixed(2)}</Text>
         <View style={styles.quantityContainer}>
-          <TouchableOpacity onPress={() => updateQuantity(item.id, false)}>
+          <TouchableOpacity onPress={() => {}}>
             <Ionicons name="remove-circle-outline" size={24} color="gray" />
           </TouchableOpacity>
-          <Text style={styles.quantityText}>{item.quantity}</Text>
-          <TouchableOpacity onPress={() => updateQuantity(item.id, true)}>
+          <Text style={styles.quantityText}>{item?.quantity}</Text>
+          <TouchableOpacity onPress={() => {}}>
             <Ionicons name="add-circle-outline" size={24} color="gray" />
           </TouchableOpacity>
         </View>
       </View>
       <Text style={styles.totalPrice}>
-        ${(item.price * item.quantity).toFixed(2)}
+        ${(item.quantity! * item.menuItem!.price).toFixed(2)}
       </Text>
     </View>
   );
 
-  const renderHiddenItem = (data: { item: CartItem }) => (
+  const renderHiddenItem = (data: { item: cartItemModel }) => (
     <View style={styles.rowBack}>
-      <TouchableOpacity
-        style={styles.backRightBtn}
-        onPress={() => handleDelete(data.item.id)}
-      >
+      <TouchableOpacity style={styles.backRightBtn} onPress={() => {}}>
         <Text style={styles.backText}>Remove</Text>
       </TouchableOpacity>
     </View>
@@ -89,12 +48,12 @@ const CartSummary: React.FC = () => {
   return (
     <View style={styles.container}>
       <SwipeListView
-        data={cartItems}
+        data={shoppingCartFromStore}
         renderItem={renderItem}
         renderHiddenItem={renderHiddenItem}
         rightOpenValue={-75}
         disableRightSwipe
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.menuItemId?.toString()!}
         contentContainerStyle={styles.listContentContainer}
         showsVerticalScrollIndicator={false}
       />
