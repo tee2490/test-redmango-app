e744ee4c091ca8e17a4e014fc6a31f4e058b8589
Teeradet <tee2490@hotmail.com>
2024-09-13 16:16:38 +0700

    Section 08 Client : Order Interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,17 @@
 import apiResponse from "./apiResponse";
 import cartItemModel from "./cartItemModel";
 import menuItemModel from "./menuItemModel";
+import orderDetailModel from "./orderDetailModel";
+import orderHeaderModel from "./orderHeaderModel";
 import shoppingCartModel from "./shoppingCartModel";
 import userModel from "./userModel";
 
-export type { menuItemModel, cartItemModel, shoppingCartModel,userModel,apiResponse };
+export type {
+  menuItemModel,
+  cartItemModel,
+  shoppingCartModel,
+  userModel,
+  apiResponse,
+  orderHeaderModel,
+  orderDetailModel,
+};
