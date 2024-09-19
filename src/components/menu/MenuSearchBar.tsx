975e74202264d4ece15a3529ac64537728c56534
Teeradet <tee2490@hotmail.com>
2024-09-19 11:58:46 +0700

    Design SearchBar – Delay function with debounce

--- src/components/menu/MenuSearchBar.tsx
+++ src/components/menu/MenuSearchBar.tsx
@@ -1,15 +1,35 @@
-import { FC } from "react";
-import { View, StyleSheet, TextInput } from "react-native";
+import { FC, useCallback, useState } from "react";
+import { View, StyleSheet, TextInput,Text } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import colors from "../../utils/colors";
+import { debounce } from 'lodash';
 
 interface Props {}
 
 const MenuSearchBar: FC<Props> = (props) => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [debouncedValue, setDebouncedValue] = useState<string>('');
+
+   // Create a debounced function
+   const debouncedSearch = useCallback(
+    
+    debounce((text: string) => {
+      setDebouncedValue(text);
+    }, 500), // 500 ms debounce time
+    []
+  );
+
+  // Handle input change
+  const handleChange = (text: string) => {
+    setSearchTerm(text);
+    debouncedSearch(text);
+  };
+  
   return (
     <View style={styles.container}>
       <AntDesign name="search1" size={20} color={colors.primary} />
-      <TextInput placeholder="Search here..." style={styles.textInput} />
+      <TextInput placeholder="Search here..." style={styles.textInput} onChangeText={handleChange} />
+      <Text>Debounced Value: {debouncedValue}</Text>
     </View>
   );
 };
