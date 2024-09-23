127da94ae858f0afb209267d6ef4a706e3bf3ed4
Teeradet <tee2490@hotmail.com>
2024-09-23 16:09:46 +0700

    Section 11 Client : Floating Action

--- src/components/home/MenuItemList.tsx
+++ src/components/home/MenuItemList.tsx
@@ -1,17 +1,20 @@
 import { FlatList, View } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MenuItemCard from "./MenuItemCard";
 import styles from "./MenuItemList.style";
 import { useDispatch, useSelector } from "react-redux";
 import { useGetMenuItemsQuery } from "../../redux/apis/menuItemApi";
 import { setMenuItem } from "../../redux/menuItemSlice";
-import { MainLoader } from "../../common";
+import { COLORS, MainLoader } from "../../common";
 import { menuItemModel } from "../../interfaces";
 import { RootState } from "../../redux/store";
 import { MenuCategoryList } from "../menu";
 import { SD_SortTypes } from "../../common/SD";
+import { FloatingAction } from "react-native-floating-action";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function MenuItemList() {
+  const flatListRef = useRef<FlatList>(null);
   const [fetching, setFetching] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState<string>("All");
   const [categoryList, setCategoryList] = useState([""]);
@@ -101,20 +104,33 @@ export default function MenuItemList() {
         }
       });
       setCategoryList(tempCategoryList);
-    }
-    else
-    {
+    } else {
       //เมื่อเกิด CRUD ทำการปรับข้อมูลให้เป็นปัจจุบัน
-      data && setMenuItems(data.result)
+      data && setMenuItems(data.result);
     }
-  }, [isLoading,data]);
+  }, [isLoading, data]);
 
   if (isLoading) {
     return <MainLoader />;
   }
 
+  const actions = [
+    {
+      icon: <AntDesign name="totop" size={24} color={COLORS.white} />,
+      text: "Go to Top",
+      name: "bt_go_to_top",
+      position: 1,
+    },
+  ];
+
+  const scrollToTop = () => {
+    if (flatListRef.current) {
+      flatListRef.current.scrollToOffset({ animated: true, offset: 0 });
+    }
+  };
+
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <View style={styles.categoryContainer}>
         <MenuCategoryList
           categoryList={categoryList}
@@ -123,13 +139,25 @@ export default function MenuItemList() {
       </View>
       <View style={styles.container}>
         <FlatList
+          ref={flatListRef}
           refreshing={fetching}
           onRefresh={onRefreshData}
+          keyExtractor={(item, index) => index.toString()}
           data={menuItems}
           numColumns={2}
           renderItem={({ item }) => <MenuItemCard menuItem={item} />}
           contentContainerStyle={styles.container}
           ItemSeparatorComponent={() => <View style={styles.separator} />}
+        />
+
+        <FloatingAction
+          distanceToEdge={{ vertical: 55, horizontal: 20 }}
+          actions={actions}
+          onPressItem={(name) => {
+            if (name === "bt_go_to_top") {
+              scrollToTop();
+            }
+          }}
         />
       </View>
     </View>
--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -18,6 +18,7 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    flex:1,
     paddingLeft: SIZES.small / 2,
     backgroundColor: COLORS.white,
   },
