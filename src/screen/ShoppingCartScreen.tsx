0076a9b3a88a175feffd6bb31455de3ce9b912e0
Teeradet <tee2490@hotmail.com>
2024-09-26 16:38:58 +0700

    Section 11 Client : Re design ShoppingCartScreen

--- src/screen/ShoppingCartScreen.tsx
+++ src/screen/ShoppingCartScreen.tsx
@@ -1,23 +1,35 @@
 import { View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { CartPickUpDetails, CartSummary } from "../components/cart";
-import {
-  GestureHandlerRootView,
-} from "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { SIZES } from "../common";
+import { COLORS, SIZES } from "../common";
+import { FormButton1 } from "../ui";
 
 export default function ShoppingCartScreen() {
+  const [visible, setVisible] = useState(true);
   return (
     <>
-    {/* ใส่ครอบ GestureHandlerRootView เพื่อให้เลื่อน Scroll+SwipeList ได้ */}
+      {/* ใส่ครอบ GestureHandlerRootView เพื่อให้เลื่อน Scroll+SwipeList ได้ */}
       <GestureHandlerRootView>
         <View style={{ flex: 1 }}>
-          <View style={{ flex: 0.5,padding: SIZES.xSmall-2, backgroundColor:'#24B0AC',borderRadius:SIZES.xSmall}}>
-            <CartSummary />
-          </View> 
-          <View style={{ flex: 0.5,padding:SIZES.xSmall-2, backgroundColor:'#F98028',borderRadius:SIZES.xSmall}}>
-            <CartPickUpDetails />
+          <View style={{ height: 50 }}>
+            <FormButton1
+              color={visible ? COLORS.primary : COLORS.danger}
+              isValid={true}
+              title={visible ? "CartSummary >>>" : "<<< CartPickUpDetails"}
+              onPress={() => setVisible(!visible)}
+            />
+          </View>
+          <View
+            style={{
+              flex: 1,
+              padding: SIZES.xSmall - 2,
+              backgroundColor: "#24B0AC",
+              borderRadius: SIZES.xSmall,
+            }}
+          >
+            {visible ? <CartSummary /> : <CartPickUpDetails />}
           </View>
         </View>
       </GestureHandlerRootView>
